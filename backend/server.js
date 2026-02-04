f9d69e41ea2d511f46ca9c4fdd5a5a644da983f6
MACKOzKLANU <sek-maciek@o2.pl>
2026-02-04 11:47:30 +0100

    develop backend

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,20 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from "cookie-parser";
+
+dotenv.config();
 
 const app = express();
 
-const port = 3000;
+app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
     res.send("Hello world");
 })
 
-app.listen(port, () => {
-    console.log("Server is running on port 5000");
-})
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+});
